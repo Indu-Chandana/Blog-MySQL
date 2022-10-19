15c1d2553a814e08619d34ee17a016e1352de87e
Indu-Chandana <indu.99.ic@gmail.com>
2022-10-19 07:07:11 +0530

    Oct19-001-react stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,63 @@
-function App() {
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import "./style.scss"
+const Layout = () => {
   return (
-    <div>
-      Hello world!
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/post/:id",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="container">
+      <RouterProvider router={router} />
+      </div>
     </div>
+
   );
 }
 
