71816aa689d3813491e9c45b12d1f80b7cfaa763
Indu-Chandana <indu.99.ic@gmail.com>
2022-10-20 08:18:32 +0530

    Oct20-001-reg page design

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <div className='auth'>
+            <h1>Register</h1>
+            <form>
+                <input type="text" placeholder='username' />
+                <input type="email" placeholder='username' />
+                <input type="password" placeholder='password' />
+                <button>Register</button>
+                <p>This is an error!</p>
+                <span>Do you have an account? <Link to="/login">Login</Link></span>
+            </form>
+        </div>
+    )
 }
 
 export default Register
