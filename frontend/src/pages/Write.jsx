5bd23493eaf5204f4d13c0fc31900e362b7d10d5
Indu-Chandana <indu.99.ic@gmail.com>
2022-10-27 06:59:43 +0530

    Oct27-002

--- frontend/src/pages/Write.jsx
+++ frontend/src/pages/Write.jsx
@@ -12,6 +12,7 @@ const Write = () => {
         <ReactQuill theme="snow" value={value} onChange={setValue} />
         </div>
       </div>
+      
       <div className='menu'>
         <div className='item'>
           <h1>Publish</h1>
@@ -32,16 +33,16 @@ const Write = () => {
           <h1>Category</h1>
           <input type="radio" name='cat' value='art' id='art'/>
           <label htmlFor="art">Art</label>
-          <input type="radio" name='cat' value='art' id='art'/>
-          <label htmlFor="art">Art</label>
-          <input type="radio" name='cat' value='art' id='art'/>
-          <label htmlFor="art">Art</label>
-          <input type="radio" name='cat' value='art' id='art'/>
-          <label htmlFor="art">Art</label>
-          <input type="radio" name='cat' value='art' id='art'/>
-          <label htmlFor="art">Art</label>
-          <input type="radio" name='cat' value='art' id='art'/>
-          <label htmlFor="art">Art</label>
+          <input type="radio" name='cat' value='science' id='science'/>
+          <label htmlFor="science">Science</label>
+          <input type="radio" name='cat' value='technology' id='technology'/>
+          <label htmlFor="technology">Technology</label>
+          <input type="radio" name='cat' value='cinema' id='cinema'/>
+          <label htmlFor="cinema">Cinema</label>
+          <input type="radio" name='cat' value='design' id='design'/>
+          <label htmlFor="design">Design</label>
+          <input type="radio" name='cat' value='food' id='food'/>
+          <label htmlFor="food">Food</label>
         </div>
       </div>
     </div>
