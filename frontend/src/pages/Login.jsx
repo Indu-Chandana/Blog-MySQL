2590c81994dfbc93d705765a3f6013e39a73bead
Indu-Chandana <indu.99.ic@gmail.com>
2022-11-02 06:25:18 +0530

    Nov02-001

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,38 @@
+import axios from 'axios';
 import React from 'react'
-import {Link} from "react-router-dom";
+import { useState } from 'react';
+import {Link, useNavigate} from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [input, setInput] = useState({
+        username: "",
+        password: "",
+    });
+    const [err, setError] = useState(null);
+
+    const handleChange = (e) => {
+        setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post("/auth/login", input);
+            navigate("/");
+        } catch (error) {
+            setError(error.response.data);
+        }
+    }
     return (
         <div className='auth'>
             <h1>Login</h1>
             <form>
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='password' />
-                <button>Login</button>
-                <p>This is an error!</p>
+                <input type="text" placeholder='username' name='username' onChange={handleChange}/>
+                <input type="password" placeholder='password' name='password' onChange={handleChange}/>
+                <button onClick={handleSubmit}>Login</button>
+                {err && <p>{err}</p>}
                 <span>Don't you have an account? <Link to="/register">Register</Link></span>
             </form>
         </div>
