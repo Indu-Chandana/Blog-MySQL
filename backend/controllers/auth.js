40c9da0e0db35fc9191067ba58ef34dfb3c43034
Indu-Chandana <indu.99.ic@gmail.com>
2022-11-02 06:44:17 +0530

    Nov02-002

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 export const register = (req, res) => {
 
     //CHECK EXISTING USER
-    const q = "SELECT * FROM users WHERE email = ? OR username = ?"
+    const q = "SELECT * FROM users WHERE email = ? OR username = ?"  // users mean tablename
 
     db.query(q, [req.body.email, req.body.name], (err, data) => {
         if (err) return res.json(err);
@@ -12,7 +12,7 @@ export const register = (req, res) => {
 
         //Hash the password and create a user
         const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password , salt);
+        const hash = bcrypt.hashSync(req.body.password, salt);
 
         const q = "INSERT INTO users(`username`, `email`,`password`) VALUES (?)"
         const values = [
@@ -21,7 +21,7 @@ export const register = (req, res) => {
             hash
         ]
 
-        db.query(q,[values], (err, data) => {
+        db.query(q, [values], (err, data) => {
             if (err) return res.json(err);
             return res.status(200).json("User has been created.");
         })
@@ -29,6 +29,17 @@ export const register = (req, res) => {
 }
 
 export const login = (req, res) => {
+    //CHECK USER
+    const q = "SELECT * FROM users WHERE username = ?" // users mean tablename
+    db.query(q, [req.body.username], (err, data) => {
+        if (err) return res.json(err);
+        if (data.length === 0) return res.status(404).json("User not found!")
+
+        //CHECK password
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password); // trueorfalse data[0].password->server saved pw and req.body.password -> user FE typed pw
+        if (!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
+    })
+
 
 }
 
