99deaf5761cc0a5f782161fecca27ca7a40ac356
Indu-Chandana <indu.99.ic@gmail.com>
2022-11-06 05:22:17 +0530

    Nov06-001

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,4 +1,5 @@
 import {db} from "../db.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
     const q = req.query.cat 
@@ -6,7 +7,7 @@ export const getPosts = (req, res) => {
     : "SELECT * FROM posts";
 
     db.query(q, [req.query.cat], (err, data) => {
-        if (err) return res.send(err);
+        if (err) return res.status(500).send(err);
 
         return res.status(200).json(data);
     })
@@ -17,7 +18,7 @@ export const getPost = (req, res) => {
     const q = "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ? "
 
     db.query(q, [req.params.id], (err, data) => {
-        if (err) return res.send(err);
+        if (err) return res.status(500).json(err);
 
         console.log('data', data)
         return res.status(200).json(data[0]);
