3622a0fa607ff9b9d67bd5512959c5714a24ddde
Indu-Chandana <indu.99.ic@gmail.com>
2022-11-05 22:30:02 +0530

    Nov05-005

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -14,16 +14,28 @@ export const getPosts = (req, res) => {
 
 export const getPost = (req, res) => {
 
-    const q = "SELECT `username`, `title`, `desc`, `img`, `cat`, `date` FROM users u JOIN posts p ON u.id===p.uid WHERE p.id = ? "
-    // const q = "SELECT * FROM posts WHERE id=?"
+    const q = "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ? "
 
-    db.query(q, [req.params], (err, data) => {
+    db.query(q, [req.params.id], (err, data) => {
         if (err) return res.send(err);
 
-        return res.status(200).json(data);
+        console.log('data', data)
+        return res.status(200).json(data[0]);
+
+        // we get array thats why we get 0 index of the array
+        //[
+        //     RowDataPacket {
+        //       username: 'Indu',
+        //       title: 'first',
+        //       desc: 'hello',
+        //       img: 'https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png',
+        //       cat: 'art',
+        //       date: null
+        //     }
+        //   ]
     })
     
-}
+} 
 
 export const addPost = (req, res) => {
     
--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Menu from '../components/Menu';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -14,7 +14,8 @@ const Single = () => {
 
   const [post, setPost] = useState({});
 
-  const location = useLocation()
+  const location = useLocation();
+  const navigate = useNavigate();
   const postId = location.pathname.split("/")[2]
 
   const { currentUser } = useContext(AuthContext)
@@ -32,31 +33,41 @@ const Single = () => {
     fetchData();
 
   }, [postId])
-  //
+
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/")
+    } catch (error) {
+      console.log('handle delete err:', error);
+    }
+  }
+
   return (
     <div className='single'>
       <div className='content'>
         <img src={post?.img} alt="" />
         <div className="user">
-          <img src="https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png" alt="" />
+          {post?.userImg && <img src={post?.userImg} alt="" />}
           <div className='info'>
-            <span>{post.username}</span>
+            <span>{post?.username}</span>
             <p>Posted {moment(post.date).fromNow()}</p>
           </div>
 
-          {currentUser.username === post.username && (
+          {currentUser?.username === post?.username && (
             <div className="edit">
               <Link to={`/write?edit=2`}>
                 <img src={Edit} alt="" />
               </Link>
 
-              <img src={Delete} alt="" />
+              <img onClick={handleDelete} src={Delete} alt="" />
             </div>
           )}
         </div>
-        <h1>{post.title}</h1>
-       {/* We are using rich editor. It allready has <P></P> tag */}
-       {post.deac} 
+        <h1>{post?.title}</h1>
+        {/* We are using rich editor. It allready has <P></P> tag */}
+        {post?.deac}
       </div>
 
       <Menu />
