cb23b70a1374206aad91c2425cfd790e0e2e347e
Indu-Chandana <indu.99.ic@gmail.com>
2022-11-05 16:55:07 +0530

    Nov05-004

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -13,6 +13,15 @@ export const getPosts = (req, res) => {
 }
 
 export const getPost = (req, res) => {
+
+    const q = "SELECT `username`, `title`, `desc`, `img`, `cat`, `date` FROM users u JOIN posts p ON u.id===p.uid WHERE p.id = ? "
+    // const q = "SELECT * FROM posts WHERE id=?"
+
+    db.query(q, [req.params], (err, data) => {
+        if (err) return res.send(err);
+
+        return res.status(200).json(data);
+    })
     
 }
 
