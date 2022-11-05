5deba9e71054d00859c488caa674f3f19687a06e
Indu-Chandana <indu.99.ic@gmail.com>
2022-11-05 15:43:51 +0530

    Nov05-003

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,33 +1,53 @@
+import axios from 'axios';
 import React from 'react'
-import { Link } from "react-router-dom";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link, useLocation } from "react-router-dom";
 
 const Home = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "hello hello hello",
-      desc: "hello hello hello hello hello hello hello hello hello",
-      img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
-    },
-    {
-      id: 2,
-      title: "hello hello hello",
-      desc: "hello hello hello hello hello hello hello hello hello",
-      img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
-    },
-    {
-      id: 3,
-      title: "hello hello hello",
-      desc: "hello hello hello hello hello hello hello hello hello",
-      img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
-    },
-    {
-      id: 4,
-      title: "hello hello hello",
-      desc: "hello hello hello hello hello hello hello hello hello",
-      img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
-    },
-  ]
+
+  const [posts, setPosts] = useState([]);
+  const cat = useLocation().search
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts${cat}`)
+        setPosts(res.data)
+      } catch (error) {
+        console.log('err fetchData', error)
+      }
+    }
+
+    fetchData();
+
+  }, [cat])
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "hello hello hello",
+  //     desc: "hello hello hello hello hello hello hello hello hello",
+  //     img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "hello hello hello",
+  //     desc: "hello hello hello hello hello hello hello hello hello",
+  //     img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "hello hello hello",
+  //     desc: "hello hello hello hello hello hello hello hello hello",
+  //     img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "hello hello hello",
+  //     desc: "hello hello hello hello hello hello hello hello hello",
+  //     img: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png"
+  //   },
+  // ]
 
   return (
     <div className='home'>
@@ -48,7 +68,7 @@ const Home = () => {
 
           </div>
         ))}
-        
+
       </div>
     </div>
   )
--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -1,39 +1,62 @@
 import React from 'react'
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Menu from '../components/Menu';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import moment from "moment"
+import { useContext } from 'react';
+import { AuthContext } from '../context/authContext';
 
 const Single = () => {
+
+  const [post, setPost] = useState({});
+
+  const location = useLocation()
+  const postId = location.pathname.split("/")[2]
+
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData();
+
+  }, [postId])
   //
   return (
     <div className='single'>
       <div className='content'>
-        <img src="https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png" alt="" />
+        <img src={post?.img} alt="" />
         <div className="user">
           <img src="https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png" alt="" />
           <div className='info'>
-            <span>Jhon</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
 
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt="" />
+              </Link>
 
-            <img src={Delete} alt="" />
-          </div>
+              <img src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>hello hello hello hello hello hello hello hello hello hello hello</h1>
-        <p> hello hello hello hello hello hello hello hello hello hello hello
-          hello hello hello hello hello hello hello hello hello hello hello
-          hello hello hello hello hello hello hello hello hello hello helloscsdacdsvsscd
-          hello hello hello hello hello hello hello hello hello hello hello
-          hello hello hello hello hello hello hello hello hello hello hello
-          hello hello hello hello hello hello hello hello hello hello hello
-
-        </p>
+        <h1>{post.title}</h1>
+       {/* We are using rich editor. It allready has <P></P> tag */}
+       {post.deac} 
       </div>
 
       <Menu />
